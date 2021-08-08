75251f75da025ebe865b60e29813e86cde186351
Dylan Howard <dylanhoward0424@outlook.com>
2021-08-07 22:24:22 -0500

    styled the table and finished refreshTasks function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,11 +8,45 @@ function onReady() {
     $(document).on('click', '.deleteBtn', removeTask);
 
     // append current database info to DOM on page load
-    displayTasks();
+    refreshTasks();
 }
 
-function displayTasks() {
+function refreshTasks() {
     console.log('Ready to display some tasks!');
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then((res) => {
+        console.log(res);
+        renderTasks(res);
+    }).catch((err) => {
+        console.log('error in GET /tasks', err);
+    });
+}
+
+function renderTasks(tasks) {
+    console.log('Rendering all of the tasks, all of the time');
+
+    // empty the current tasks list
+    $('#taskList').empty();
+
+    for (let task of tasks) {
+
+        // for each task, append a new row to our table
+        $('#taskList').append(`
+            <td class="taskColumns">
+                ${task.task}
+            </td>
+            <td>${task.due}</td>
+            <td>${task.isCompleted}</td>
+            <td>
+                <button class="completeBtn">Complete</button>
+            </td>
+            <td>
+                <button class="deleteBtn">Delete</button>
+            </td>
+        `);
+    }
 }
 
 function addTask() {
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -5,5 +5,21 @@ const pool = require('../modules/pool');
 
 // CODE WILL GO HERE
 
+// corresponds with get request on client-side
+// whenever function refreshTasks() is called
+router.get('/', (req, res) => {
+    // mimic postico code to get data from database
+    let queryText = 'SELECT * FROM "tasks" ORDER BY "due" ASC;';
+
+    // use SQL through pool.query to get the info back to client
+    pool.query(queryText).then(result => {
+        
+        // result --> information from database
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
